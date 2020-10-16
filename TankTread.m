64dabcb2c33eff7979b42731eedabed23d737e86
MeganThrolson <42652883+MeganThrolson@users.noreply.github.com>
2020-10-16 17:35:21 -0400

    structure in place

--- TankTread.m
+++ TankTread.m
@@ -1,6 +1,26 @@
 classdef TankTread
     properties
-        k
+        k_torsion
+        k_spring
         
+        A
+        X
+        B
+        
+    end
+    methods
+        function TankBuilder(obj,k_torsion, k_spring)
+            obj.k_torsion = k_torsion;
+            obj.k_spring = k_spring;
+        end
+        function solveA(obj)
+        end
+        function solveX(obj)
+        end
+        function SolveB(obj)
+        end
+        function output = Driving(obj, road)
+            output = obj.A*obj.X + obj.B*road.Input;
+        end
     end
 end
